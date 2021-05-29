355a0327b3f8aed6800dc83ace71c30bd03316d6
Metacomet10 <metacomet10@me.com>
2021-05-29 13:59:20 -0700

    Add source for VRGtR and UA:DO
    Create source definitions for Van Richten's and UA:Draconic Options

---
+++ Content/pub_20210518_VRGtR.js
@@ -0,0 +1,11 @@
+var iFileName = "pub_20210518_VRGtR.js";
+RequiredSheetVersion(13);
+
+// Define the source
+SourceList.V = {
+	name: "Van Richten's Guide to Ravenloft",
+	abbreviation: "VRGtR",
+	group: "Primary Sources",
+	url: "https://dnd.wizards.com/products/van-richtens-guide-ravenloft",
+	date: "2021/05/18"
+};
---
+++ Content/ua_20210414_Draconic-Options.js
@@ -0,0 +1,11 @@
+var iFileName = "ua_20210414_Draconic-Options.js";
+RequiredSheetVersion(13);
+
+// Define the source
+SourceList["UA:DO"] = {
+	name : "Unearthed Arcana: Draconic Options",
+	abbreviation : "UA:DO",
+	group : "Unearthed Arcana",
+	url : "https://media.wizards.com/2021/downloads/Unearthed-Arcana-2021-Draconic-Options.pdf",
+	date : "2021/04/14"
+};
