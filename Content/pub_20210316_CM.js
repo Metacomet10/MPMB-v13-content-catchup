67bf7fafd43f1e734985f1bbac0f62e2d2968293
Metacomet10 <metacomet10@me.com>
2021-06-16 12:30:30 -0700

    Create Candlekeep source definition

---
+++ Content/pub_20210316_CM.js
@@ -0,0 +1,11 @@
+var iFileName = "pub_20210316_CM.js";
+RequiredSheetVersion(13);
+
+// Define the source
+SourceList.CM = {
+	name: "Candlekeep Mysteries ",
+	abbreviation: "CM",
+	group: "Adventure Books",
+	url: "https://dnd.wizards.com/products/candlekeep-mysteries",
+	date: "2021/03/16"
+};
