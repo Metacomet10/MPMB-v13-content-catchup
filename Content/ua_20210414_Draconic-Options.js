32347468a2ca44439409dcf77f75724e951463f6
NodHero <35711894+NodHero@users.noreply.github.com>
2021-07-15 23:41:04 -0400

    Update ua_20210414_Draconic-Options.js

--- Content/ua_20210414_Draconic-Options.js
+++ Content/ua_20210414_Draconic-Options.js
@@ -57,43 +57,13 @@ RaceList["draconic kobold"] = {
 	trait : "Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 };
 
-RaceList["draconic kobold"] = {
-	regExpSearch : /kobold/i,
-	name : "Draconic Kobold",
-	source : ["UA:DO", 4],
-	plural : "Draconic Kobolds",
-	size : 4,
-	speed : {
-		walk : { spd : 30, enc : 20 }
-	},
-	languageProfs : ["Common", "Draconic"],
-	vision : [["Darkvision", 60]],
-	age : " reach adulthood at age 6 and can live up to 120 years, but rarely do so",
-	height : " are between 2 and 3 feet tall (2'1\" + 2d4\")",
-	weight : " weigh between 25 and 35 lb (25 + 2d4 \xD7 1 lb)",
-	heightMetric : " are between 65 and 90 cm tall (63 + 5d4 cm)",
-	weightMetric : " weigh between 10 and 15 kg (11 + 5d4 \xD7 2 / 10 kg)",
-	scorestxt : "+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice",
-	features : {
-		"draconic roar" : {
-			name : "Draconic Roar",
-			minlevel : 1,
-			usages : "Proficiency bonus per ",
-			action : [["bonus action", ""]],
-			usagescalc : "event.value = How('Proficiency Bonus');",
-			recovery: "long rest"
-		},
-	},
-trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice)\n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
-};
-
 AddRacialVariant("draconic kobold", "brave", {
 	regExpSearch : /brave/i,
 	name : "Draconic Kobold-Brave",
 	source : ["UA:DO", 4],
 	plural : "Draconic Kobolds",
 	savetxt : { adv_vs : ["frightened"] },
-	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 I have advantage on saving throws to avoid or end the frightened condition on myself. \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
+	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Draconic Legacy (Brave). I have advantage on saving throws to avoid or end the frightened condition on myself. \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 });
 
 AddRacialVariant("draconic kobold", "tail", {
@@ -101,7 +71,7 @@ AddRacialVariant("draconic kobold", "tail", {
 	name : "Draconic Kobold-Tail",
 	source : ["UA:DO", 4],
 	plural : "Draconic Kobolds",
-	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 I can make unarmed strikes with my tail. The Draconic Tail deals 1d6 + my Strength modifier bludgeoning damage. \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
+	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Draconic Legacy (Tail). I can make unarmed strikes with my tail. The Draconic Tail deals 1d6 + my Strength modifier bludgeoning damage. \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 	weaponOptions : {
 		baseWeapon : "unarmed strike",
 		regExpSearch : /^(?=.*draconic)(?=.*tail).*$/i,
@@ -117,7 +87,7 @@ AddRacialVariant("draconic kobold", "cantrip (Intelligence)", {
 	name : "Draconic Kobold-Cantrip",
 	source : ["UA:DO", 4],
 	plural : "Draconic Kobolds",
-	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Cantrip. I know one cantrip from the sorcerer spell list. Intelligence is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
+	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Draconic Legacy (Cantrip). I know one cantrip from the sorcerer spell list. Intelligence is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 	spellcastingBonus : {
 		name : "Draconic Legacy",
 		"class" : "sorcerer",
@@ -132,7 +102,7 @@ AddRacialVariant("draconic kobold", "cantrip (Wisdom)", {
 	name : "Draconic Kobold-Cantrip",
 	source : ["UA:DO", 4],
 	plural : "Draconic Kobolds",
-	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Cantrip. I know one cantrip from the sorcerer spell list. Wisdom is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
+	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Draconic Legacy (Cantrip). I know one cantrip from the sorcerer spell list. Wisdom is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 	spellcastingBonus : {
 		name : "Draconic Legacy",
 		"class" : "sorcerer",
@@ -147,7 +117,7 @@ AddRacialVariant("draconic kobold", "cantrip (Charisma)", {
 	name : "Draconic Kobold-Cantrip",
 	source : ["UA:DO", 4],
 	plural : "Draconic Kobolds",
-	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Cantrip. I know one cantrip from the sorcerer spell list. Charisma is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
+	trait : "Draconic Kobold (+2 to one ability score and +1 to a different score of my choice, -or- +1 to three different scores of my choice) \n \u2022 Draconic Legacy (Cantrip). I know one cantrip from the sorcerer spell list. Charisma is my spellcasting ability for that cantrip (chosen when I selected this race). \n \u2022 Draconic Roar. As a bonus action, draconic roar at enemies within 10 feet of me. Until the end of my next turn, my allies and I have advantage on attack rolls against any of those enemies who could hear the roar.",
 	spellcastingBonus : {
 		name : "Draconic Legacy",
 		"class" : "sorcerer",
